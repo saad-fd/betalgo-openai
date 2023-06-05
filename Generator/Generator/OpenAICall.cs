3643b326bb91cf24764df32a6091693381269496
Saad <saad.fadlulloh@ecomindo.com>
2023-06-05 16:36:30 +0700

    Add all codes

---
+++ Generator/Generator/OpenAICall.cs
@@ -0,0 +1,52 @@
+﻿using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using System.ComponentModel;
+
+namespace OpenAI.Playground.TestHelpers;
+
+internal static class OpenAICall
+{
+    public static async Task<string> CallOpenAIAPI(IOpenAIService sdk, string prompt)
+    {
+        string result = "";
+
+        try
+        {
+            Console.WriteLine("Generating...");
+            var completionResult = await sdk.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+            {
+                Messages = new List<ChatMessage>
+            {
+                ChatMessage.FromSystem(prompt),
+
+            },
+                MaxTokens = 2048,
+                Model = Models.ChatGpt3_5Turbo
+            });
+
+            if (completionResult.Successful)
+            {
+                result = completionResult.Choices.First().Message.Content;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                {
+                    throw new Exception("Unknown Error");
+                }
+
+                result = $"{completionResult.Error.Code}: {completionResult.Error.Message}";
+                Console.WriteLine(result);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return result;
+    }
+}
---
+++ Generator/Generator/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.Playground.TestHelpers;
+#if NET6_0_OR_GREATER
+//using LaserCatEyes.HttpClientListener;
+#endif
+
+var builder = new ConfigurationBuilder()
+    .AddJsonFile("ApiSettings.json")
+    .AddUserSecrets<Program>();
+
+IConfiguration configuration = builder.Build();
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddScoped(_ => configuration);
+
+serviceCollection.AddOpenAIService();
+
+var serviceProvider = serviceCollection.BuildServiceProvider();
+var sdk = serviceProvider.GetRequiredService<IOpenAIService>();
+
+List<string> topics = new List<string>
+        {
+            "Book", "Reserve", "Schedule", "Modify", "Change", "Cancel", "Status",
+            "Departure", "Arrival", "Delay", "Cancellation", "Update", "Allowance", "Restrictions", "Fees",
+            "Lost", "Damaged", "Class", "Assistance", "Unaccompanied", "Minors", "Account", "Miles",
+            "Redeem", "Offers", "Promotions", "Refund", "Compensation", "Policy", "Process", "Visa",
+            "Passport", "Validity", "Documents", "Check-in", "Boarding", "Online", "Mobile", "Passes",
+            "Meals", "Entertainment", "Wi-Fi", "Specials", "Pets", "Questions", "Feedback", "Complaints",
+            "Suggestions", "Discounts", "Promotions", "Medical", "Wheelchair", "Group"
+        };
+
+Random random = new Random();
+for (int i = 0; i < 2; i++)  
+{
+    string randomTopic = topics[random.Next(topics.Count)];
+
+    string prompt = $"Create an example of a conversation between customer service and a customer who ask/complain/give/inquire about {randomTopic}. The customer service is an operator of Merpati Airline. Create a minimum of 2000 words. Customer uses an informal language style, while customer service uses a formal style. The conversation should be written in JSONL format as follows:\r\n{{\"role\": \"customer\", \"message\": \"any text\"}}\r\n{{\"role\": \"cs\", \"message\": \"any text\"}}";
+
+    var apiResponse = await OpenAICall.CallOpenAIAPI(sdk, prompt);
+
+    int numbering = i + 1;
+    string formattedNumber = numbering.ToString("D3");  
+    string currentDirectory = Directory.GetCurrentDirectory();  
+    File.WriteAllText(Path.Combine(currentDirectory, $"Output/Conversation_{formattedNumber}.jsonl"), apiResponse);
+
+    Console.WriteLine($"File 'Conversation_{formattedNumber}.jsonl' has been saved");
+
+}
